4f6e69ddad39c1e244271f01d6b570c9fc1f60bf
Jessica Barnett <barnetteffect@gmail.com>
2024-03-20 15:35:00 -0400

    add rotational cipher that I did a couple days ago and didnt commit

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,9 +1,45 @@
-const alphabetStr = 'abcdefghijklmnopqrstuvwxyz';
-const alphabet = alphabetStr.split('');
+// Arrays Solution
+const rotateMap = (str, rotation) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const frontOfCipher = alphabet.slice(rotation, alphabet.length);
+  const backOfCipher = alphabet.slice(0, rotation);
+  const cipherAlphabet = frontOfCipher.concat(backOfCipher);
 
-const rotate = (str, rotation) => {
-  const cipherAlphabet = alphabetStr.slice(rotation, alphabetStr.length).concat(alphabet.slice(0, rotation)).split('');
-  return str.split('').map((char) => {
-    return cipherAlphabet[alphabet.indexOf(char)];
-  });
+  const res = str.split('').map((char) => {
+    let cipherChar = cipherAlphabet[alphabet.indexOf(char.toLowerCase())];
+
+    if (cipherChar && char.toUpperCase() === char) {
+      cipherChar = cipherChar.toUpperCase();
+    }
+    return cipherChar ?? char;
+  })
+
+  return res.join('');
+};
+
+/**
+  Trying a version using regex instead of arrays, just to see.
+   It isn't working because it's replacing the first letter, say
+   e with i, and then when it gets to i later, replaces it again.
+   I'd need to do all the replacements at once to do it this way...
+ */
+const rotateReplace = (str, rotation) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const frontOfCipher = alphabet.slice(rotation, alphabet.length);
+  const backOfCipher = alphabet.slice(0, rotation);
+  const cipherAlphabet = frontOfCipher.concat(backOfCipher);
+  console.log(cipherAlphabet)
+
+  return cipherAlphabet.reduce((rotatedStr, _, idx) => {
+    console.log(`replacing "${alphabet[idx]}" with "${cipherAlphabet[idx]}" in ${rotatedStr}`);
+    return rotatedStr
+      .replace(new RegExp(alphabet[idx], 'g'), cipherAlphabet[idx])
+      .replace(new RegExp(alphabet[idx].toUpperCase(), 'g'), cipherAlphabet[idx].toUpperCase());
+  }, str)
+};
+
+// Output
+export const rotate = (str, rotation) => {
+  return rotateMap(str, rotation);
+  // return rotateReplace(str, rotation);
 };
