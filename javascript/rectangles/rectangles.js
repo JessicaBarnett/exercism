ad554a3e8a26a05fd67df42bdd0a95d91077c0a3
Jessica Barnett <barnetteffect@gmail.com>
2024-08-02 17:38:17 -0400

    Finished first successful pass at rectangles challenge.  Needs a lot of work but it passes.

--- javascript/rectangles/rectangles.js
+++ javascript/rectangles/rectangles.js
@@ -1,36 +1,83 @@
-const testAscii = [
-  '+---+--+----+',
-  '|   +--+----+',
-  '+---+--+    |',
-  '|   +--+----+',
-  '+---+--+--+-+',
-  '+---+--+--+-+',
-  '+------+  | |',
-  '          +-+',
-]; // 60 rectangles
+//*****  GETTERS/CONVERTERS  *****//
 
-// Formatting
-// 1. split string into array by newlines (not needed b/c they're passing in arrays of rows)
-// 2. split characters into elements by character
+/**
+ * @param {string[]} asciiRows
+ * @returns {string[]}
+ * @desc splits characters into elements by character
+ */
 const asciiRowsToMatrix = (asciiRows) => {
   return asciiRows.map(row => row.split(''));
 }
 
-// Boolean checks
+/**
+ * @param {string[]} asciiMatrix array of arrays of single characters
+ * @param {number} col1 column index of a rectangle's left edge
+ * @param {number} col2 column index of a rectangle's right edge
+ * @param {number} row row index of a rectangle's top edge
+ * @returns {string[]}
+ * @desc separates out and returns the chars between col1 & col2 at row.
+ */
+const getCharsForHorizontalEdge = (asciiMatrix, col1, col2, row) => {
+  return asciiMatrix[row].slice(col1, col2+1); // returns array of chars
+}
+
+/**
+ * @param {string[]} asciiMatrix array of arrays of single characters
+ * @param {number} col column index of a rectangle's edge
+ * @param {number} row1 row index of a rectangle's top edge
+ * @param {number} row2 row index of a rectangle's bottom edge
+* @returns {string[]}
+ * @desc separates out and returns array of chars between row1 & row2 at col
+ */
+const getCharsForVerticalEdge = (asciiMatrix, col, row1, row2) => {
+  const result = [];
+  for (let r = row1; r <= row2; r++) {
+    result.push(asciiMatrix[r][col]);
+  }
+  return result;
+}
+
+//*****  BOOLEAN CHECKS  *****//
+
+/**
+ * @param {string} char
+ * @returns {Boolean}
+ * @desc checks a character to see if its a corner (+)
+ */
 const isCorner = (char) => char === '+';
 
-const hasBottomEdgeAtRow = (asciiMatrix, col1, col2, row) => {
-  return asciiMatrix[row][col1] === '+' && asciiMatrix[row][col2] == '+';
+/**
+ * @param {number} rowChars array of single-character strings representing the edge of a rectangle
+ * @returns {Boolean}
+ * @desc Returns true if chars in rowChars match the pattern: +-----+ w/ any number of, or 0, dashes
+ */
+const isCompleteRow = (rowChars) => rowChars.join('').match(/^\+[-,+]*\+$/) !== null;
+
+/**
+ * @param {number} colChars array of single-character strings representing the edge of a rectangle
+ * @returns {Boolean}
+ * @desc Returns true if chars in colChars match the pattern: +|||+ w/ any number of, or 0, pipes
+ */
+const isCompleteCol = (colChars) => colChars.join('').match(/^\+[\|,+]*\+$/) !== null;
+
+/**
+ * @param {string[]} asciiMatrix array of arrays of single characters
+ * @param {number} col1 column index of a rectangle's left edge
+ * @param {number} col2 column index of a rectangle's right edge
+ * @param {number} row1 row index of a rectangle's top edge
+ * @param {number} row2 row index of a rectangle's bottom edge
+* @returns {Boolean}
+ * @desc Returns true if all 4 edges are complete
+ */
+const isCompleteSquare = (asciiMatrix, col1, col2, row1, row2) => {
+  return isCompleteRow(getCharsForHorizontalEdge(asciiMatrix, col1, col2, row1)) &&
+    isCompleteRow(getCharsForHorizontalEdge(asciiMatrix, col1, col2, row2)) &&
+    isCompleteCol(getCharsForVerticalEdge(asciiMatrix, col1, row1, row2)) &&
+    isCompleteCol(getCharsForVerticalEdge(asciiMatrix, col2, row1, row2));
 }
 
-const hasCompleteSides = (asciiMatrix, col1, col2, topRow, bottomRow)  => {
-  const squareBits = [
-    ...topRow.slice(col1, col2-col1), 
-    ...bottomRow.slice(col1, col2-col1),
-    
-  // all elements from col1 to col2 in topRow and bottomRow are all `-`
-  // 
-}
+//*****  LOGIC  *****//
+
 
 // Logic
 // for each + in graph
@@ -49,7 +96,8 @@ const countRectanglesForCorner = (asciiMatrix, originRow, originCol) => {
       // iterate rows below
       for (let row = originRow+1; row < asciiMatrix.length; row++) {
         // if the origin and sibling columns both have a `+` in the current row...
-        if (hasBottomEdgeAtRow(asciiMatrix, originCol, siblingCol, row)) {
+        // debug(asciiMatrix, originCol, siblingCol, originRow, row)
+        if (isCompleteSquare(asciiMatrix, originCol, siblingCol, originRow, row)) {
           // add a rectanlge
           cornerRectangles += 1;
         }
@@ -75,12 +123,38 @@ const countRectanglesInMatrix = (asciiRows) => {
   return matrixRectangles;
 }
 
-// console.log(countRectanglesInMatrix(testAscii));
-
 /**
  *
  * @param {*} asciiRows array where each element is a row of an ascii drawing
  */
 export function count(asciiRows) {
   return countRectanglesInMatrix(asciiRows);
+}
+
+/** DEBUG **/
+
+function debug (asciiMatrix, col1, col2, row1, row2, res) {
+  const topEdge = getCharsForHorizontalEdge(asciiMatrix, col1, col2, row1),
+        bottomEdge = getCharsForHorizontalEdge(asciiMatrix, col1, col2, row2),
+        leftEdge = getCharsForVerticalEdge(asciiMatrix, col1, row1, row2),
+        rightEdge = getCharsForVerticalEdge(asciiMatrix, col2, row1, row2);
+
+  console.log(`
+Matrix:
+${asciiMatrix.map(r => r.join('')).join('\n')}
+points: (${row1}, ${col1}) / (${row2}, ${col2})
+${isCompleteSquare(asciiMatrix, col1, col2, row1, row2) ? 'IS NOT a square' : 'IS a square'}
+topEdge:
+isComplete: ${isCompleteRow(topEdge)}
+characters: "${topEdge.join('')}"
+bottomEdge:
+isComplete: ${isCompleteRow(bottomEdge)}
+characters: "${bottomEdge.join('')}"
+leftEdge:
+isComplete: ${isCompleteCol(leftEdge)}
+characters: "${leftEdge.join('')}"
+rightEdge:
+isComplete: ${isCompleteCol(rightEdge)}
+characters: "${rightEdge.join('')}"
+`);
 }
